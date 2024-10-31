d70c0703aa020752669a210c047ebd502c59ef42
j.xnah7 <jonahsendejo@gmail.com>
2024-10-30 23:15:26 -0500

    trying-stacks

---
+++ stacks/tryOut.py
@@ -0,0 +1,36 @@
+pancakes = []
+
+pancakes.append('bottom')
+pancakes.append("middle")
+pancakes.append('top')
+pancakes.append('syrup')
+
+
+
+print(pancakes)
+
+print("wait no me want syrup pop that azz")
+
+pancakes.pop()
+
+print(pancakes)
+
+print("Mucho bettero")
+
+pancakes.append('butter')
+pancakes.append('dynamite')
+
+print('pancakes are gr8 imma eat the middle one first tho so pop ittttt')
+
+pancakes.pop(1)
+
+print(pancakes)
+
+print('interesting, can u remove da butta and dynamite plsplspls')
+
+pancakes.pop(2)
+pancakes.pop(2)
+
+print(pancakes)
+
+print('yummy thx youuu')
